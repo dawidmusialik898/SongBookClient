df91a253baecb0cd3dbc070ee56fd6ff7015f123
Dawid Musialik <dawidmusialik1998@gmail.com>
2026-07-07 18:13:27 +0200

    add server package

--- src/factories.js
+++ src/factories.js
@@ -1,15 +1,24 @@
-
 function btnFactory(name, onClickFunc, cssClass) {
-    const button= document.createElement("button")
+    const button = document.createElement("button")
     const text = document.createTextNode(name)
     button.appendChild(text)
     button.className = cssClass
 
     // Add a 'click' event to that button element.
     button.addEventListener("click", onClickFunc)
+    divFactory()
     return button
 }
 
 function divFactory() {
+    let z = new Point()
+    z.x = "hello world"
+    z.y = 5
+    console.log(z.x)
+    console.log(z.y)
 }
 
+class Point {
+    x
+    y
+}
